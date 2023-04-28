5201440184920b3b43453ab7a53451a9c9719a69
EniolaADemola <it.eniolaademola@gmail.com>
2023-04-28 03:01:52 -0700

    add assets folder

---
+++ src/asssets/data.js
@@ -0,0 +1,140 @@
+import Book1 from "../asssets/pdfs/test1.pdf";
+import Book2 from "../asssets/pdfs/test2.pdf";
+import Book3 from "../asssets/pdfs/test3.pdf";
+import Book4 from "../asssets/pdfs/test4.pdf";
+import Book5 from "../asssets/pdfs/test5.pdf";
+import Book6 from "../asssets/pdfs/test6.pdf";
+import Book7 from "../asssets/pdfs/test7.pdf";
+import Book8 from "../asssets/pdfs/test8.pdf";
+import Book9 from "../asssets/pdfs/test9.pdf";
+import Book10 from "../asssets/pdfs/test10.pdf";
+
+export const Books = [
+  {
+    id: 1,
+    href: Book1,
+    title: "Mathematics",
+    authorName: "Eniola Ademola",
+    numOfPages: 355,
+  },
+  {
+    id: 2,
+    href: Book2,
+    title: "Medical Biochemistry",
+    authorName: "Oyaronbi Oyinkansola",
+    numOfPages: 455,
+  },
+  {
+    id: 3,
+    href: Book3,
+    title: "The History of Scapular",
+    authorName: "DAVEWORLD",
+    numOfPages: 55,
+  },
+  {
+    id: 4,
+    href: Book4,
+    title: "Introduction to Programming",
+    authorName: "Eniola David",
+    numOfPages: 355,
+  },
+  {
+    id: 5,
+    href: Book5,
+    title: "Complete Python Program Book",
+    authorName: "Eniola Ademola",
+    numOfPages: 239,
+  },
+  {
+    id: 6,
+    href: Book6,
+    title: "GNS201",
+    authorName: "Adedeji Ayomide",
+    numOfPages: 355,
+  },
+  {
+    id: 7,
+    href: Book7,
+    title: "Who is God?",
+    authorName: "Ifeoluwa Grace",
+    numOfPages: 355,
+  },
+  {
+    id: 8,
+    href: Book8,
+    title: "Mental Health",
+    authorName: " Ademola",
+    numOfPages: 23,
+  },
+  {
+    id: 9,
+    href: Book9,
+    title: "100 Days with Jesus",
+    authorName: "David Oyakilome",
+    numOfPages: 355,
+  },
+  {
+    id: 10,
+    href: Book10,
+    title: "99 Days for the Thief",
+    authorName: "DAVEWORLD",
+    numOfPages: 102,
+  },
+];
+
+export const QuoteWords = [
+  {
+    id: 1,
+    quote: "The purpose of our lives is to be happy",
+    author: " Dalai Lama",
+  },
+  {
+    id: 2,
+    quote: "Life is what happens when you're busy making other plans",
+    author: "John Lennon",
+  },
+  {
+    id: 3,
+    quote: "Get busy living or get busy dying.",
+    author: "Stephen King",
+  },
+  {
+    id: 4,
+    quote: "You only live once, but if you do it right, once is enough",
+    author: "Mae West",
+  },
+  {
+    id: 5,
+    quote:
+      "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+    author: " Thomas A. Edison",
+  },
+  {
+    id: 6,
+    quote:
+      "If you want to live a happy life, tie it to a goal, not to people or things.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 7,
+    quote: "Never let the fear of striking out keep you from playing the game.",
+    author: " Babe Ruth",
+  },
+  {
+    id: 8,
+    quote:
+      "Money and success don’t change people; they merely amplify what is already there.",
+    author: "Will Smith",
+  },
+  {
+    id: 9,
+    quote:
+      "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 10,
+    quote: "Not how long, but how well you have lived is the main thing.",
+    author: "Seneca",
+  },
+];
