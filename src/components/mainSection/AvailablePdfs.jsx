28027503a4089703f9686b319b01c458105faef7
Simlex <gettosimlex@gmail.com>
2023-05-15 08:17:50 +0100

    Fixed filterdropdown Ui

--- src/components/mainSection/AvailablePdfs.jsx
+++ src/components/mainSection/AvailablePdfs.jsx
@@ -1,19 +1,139 @@
-import React from "react";
-import { Download, Eye, Filter } from "../../asssets/icon";
+import React, { useState } from "react";
+import {
+  CaretDownIcon,
+  CaretUpIcon,
+  Download,
+  Eye,
+  Filter,
+} from "../../asssets/icon";
 import { Books } from "../../asssets/data";
 
 const AvailablePdfs = ({ query, setQuery }) => {
   const filteredPDFs = Books.filter(({ title }) => {
     return title.toLowerCase().includes(query.toLowerCase());
   });
+
   // console.log(filteredPDFs);//log to see filtered result
+
+  const [filterDropdownIsVisible, setFilterDropdownIsVisible] = useState(false);
+  const [filterOptionsIsVisible, setFilterOptionsIsVisible] = useState(false);
+  const [filterOptionSelected, setFilterOptionSelected] = useState(undefined);
+
+  const filterOptions = {
+    schoolBooks: 1,
+    churchBooks: 2,
+    faculty: 3,
+    department: 4,
+  };
+
+  const switchFilterName = () => {
+    if (!filterOptionSelected) {
+      return `Select option`;
+    }
+    if (filterOptionSelected == filterOptions.schoolBooks) {
+      return "School books";
+    }
+    if (filterOptionSelected == filterOptions.churchBooks) {
+      return "Church books";
+    }
+    if (filterOptionSelected == filterOptions.department) {
+      return "Department";
+    }
+    if (filterOptionSelected == filterOptions.faculty) {
+      return "Faculty";
+    }
+  };
+
+  const switchSerchPlaceholder = () => {
+    if (filterOptionSelected == filterOptions.schoolBooks) {
+      return "Search school books...";
+    }
+    if (filterOptionSelected == filterOptions.churchBooks) {
+      return "Search church books...";
+    }
+    if (filterOptionSelected == filterOptions.department) {
+      return "Search based on department...";
+    }
+    if (filterOptionSelected == filterOptions.faculty) {
+      return "Search based on faculty...";
+    }
+  };
+
   return (
     <div className="available__pdfs">
       <div className="available__pdfs-heading">
         <h4>All available PDF's</h4>
-        <button>
-          Filter <Filter />
-        </button>
+        <div className="cta">
+          <button
+            onClick={() => setFilterDropdownIsVisible(!filterDropdownIsVisible)}
+          >
+            Filter <Filter />
+          </button>
+          {filterDropdownIsVisible && (
+            <div className="cta__dropdown">
+              <div className="filterArea">
+                <p>Filter by:</p>
+                <div className="filterContainer">
+                  <span
+                    onClick={() =>
+                      setFilterOptionsIsVisible(!filterOptionsIsVisible)
+                    }
+                  >
+                    {switchFilterName()}{" "}
+                    {filterOptionsIsVisible ? (
+                      <CaretUpIcon />
+                    ) : (
+                      <CaretDownIcon />
+                    )}
+                  </span>
+                  {filterOptionsIsVisible && (
+                    <div className="filterOptions">
+                      <span
+                        className="filterOption"
+                        onClick={() => {
+                          setFilterOptionSelected(filterOptions.schoolBooks);
+                          setFilterOptionsIsVisible(false);
+                        }}
+                      >
+                        School books
+                      </span>
+                      <span
+                        className="filterOption"
+                        onClick={() => {
+                          setFilterOptionSelected(filterOptions.churchBooks);
+                          setFilterOptionsIsVisible(false);
+                        }}
+                      >
+                        Church books
+                      </span>
+                      <span
+                        className="filterOption"
+                        onClick={() => {
+                          setFilterOptionSelected(filterOptions.faculty);
+                          setFilterOptionsIsVisible(false);
+                        }}
+                      >
+                        Faculty
+                      </span>
+                      <span
+                        className="filterOption"
+                        onClick={() => {
+                          setFilterOptionSelected(filterOptions.department);
+                          setFilterOptionsIsVisible(false);
+                        }}
+                      >
+                        Department
+                      </span>
+                    </div>
+                  )}
+                </div>
+              </div>
+              {filterOptionSelected && (
+                <input type="text" placeholder={switchSerchPlaceholder()} />
+              )}
+            </div>
+          )}
+        </div>
       </div>
       <div className="bookpdfs">
         {filteredPDFs.map(({ id, title, href, authorName, numOfPages }) => {
