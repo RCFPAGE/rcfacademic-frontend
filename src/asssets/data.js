92fcb54bdabc18aced3fdb27aaf4c63755f32ce7
EniolaADemola <it.eniolaademola@gmail.com>
2023-06-22 20:42:55 +0100

    add more quotes

--- src/asssets/data.js
+++ src/asssets/data.js
@@ -103,6 +103,11 @@ export const Books = [
 
 export const QuoteWords = [
   {
+    id: 0,
+    quote: "Not how long, but how well you have lived is the main thing.",
+    author: "Seneca",
+  },
+  {
     id: 1,
     quote: "The purpose of our lives is to be happy",
     author: " Dalai Lama",
@@ -153,7 +158,507 @@ export const QuoteWords = [
   },
   {
     id: 10,
-    quote: "Not how long, but how well you have lived is the main thing.",
-    author: "Seneca",
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: 11,
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    id: 12,
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    id: 13,
+    quote:
+      "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
+    author: "Helen Keller",
+  },
+  {
+    id: 14,
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: 15,
+    quote:
+      "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God.",
+    author: "Philippians 4:6 (Bible)",
+  },
+  {
+    id: 16,
+    quote:
+      "Faith is taking the first step even when you don't see the whole staircase.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    id: 17,
+    quote:
+      "Love your enemies, do good to those who hate you, bless those who curse you, pray for those who mistreat you.",
+    author: "Luke 6:27-28 (Bible)",
+  },
+  {
+    id: 18,
+    quote: "Be the change that you wish to see in the world.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    id: 19,
+    quote:
+      "In the end, it's not the years in your life that count. It's the life in your years.",
+    author: "Abraham Lincoln",
+  },
+  {
+    id: 20,
+    quote:
+      "Trust in the LORD with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.",
+    author: "Proverbs 3:5-6 (Bible)",
+  },
+  {
+    id: 21,
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    id: 22,
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    id: 23,
+    quote:
+      "Do all the good you can, for all the people you can, in all the ways you can, as long as ever you can.",
+    author: "John Wesley",
+  },
+  {
+    id: 24,
+    quote:
+      "Happiness is not something ready-made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    id: 25,
+    quote:
+      "Let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up.",
+    author: "Galatians 6:9 (Bible)",
+  },
+  {
+    id: 26,
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: 27,
+    quote:
+      "Keep your face always toward the sunshine—and shadows will fall behind you.",
+    author: "Walt White",
+  },
+  {
+    id: 28,
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    id: 29,
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    id: 30,
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 31,
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    id: 32,
+    quote:
+      "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Christian D. Larson",
+  },
+  {
+    id: 33,
+    quote:
+      "The function of prayer is not to influence God, but rather to change the nature of the one who prays.",
+    author: "Søren Kierkegaard",
+  },
+  {
+    id: 34,
+    quote:
+      "When you have exhausted all possibilities, remember this: you haven't.",
+    author: "Thomas Edison",
+  },
+  {
+    id: 35,
+    quote: "Don't let yesterday take up too much of today.",
+    author: "Will Rogers",
+  },
+  {
+    id: 36,
+    quote:
+      "The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart.",
+    author: "Helen Keller",
+  },
+  {
+    id: 37,
+    quote:
+      "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer",
+  },
+  {
+    id: 38,
+    quote:
+      "Faith is not the belief that God will do what you want. It is the belief that God will do what is right.",
+    author: "Max Lucado",
+  },
+  {
+    id: 39,
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 40,
+    quote: "When you focus on the good, the good gets better.",
+    author: "Abraham Hicks",
+  },
+  {
+    id: 41,
+    quote: "Let all that you do be done in love.",
+    author: "1 Corinthians 16:14 (Bible)",
+  },
+  {
+    id: 42,
+    quote:
+      "Success is not the absence of failure; it's the persistence through failure.",
+    author: "Aisha Tyler",
+  },
+  {
+    id: 43,
+    quote: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe",
+  },
+  {
+    id: 44,
+    quote:
+      "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.",
+    author: "Proverbs 3:5-6 (Bible)",
+  },
+  {
+    id: 45,
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: 46,
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    id: 47,
+    quote: "You don't have to be great to start, but you have to start",
+    author: "",
+  },
+  {
+    id: 48,
+    quote:
+      "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    author: "Roy T. Bennett",
+  },
+  {
+    id: 49,
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 50,
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    id: 51,
+    quote:
+      "Faith is taking the first step even when you don't see the whole staircase.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    id: 52,
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: 53,
+    quote: "Life is 10% what happens to us and 90% how we react to it.",
+    author: "Charles R. Swindoll",
+  },
+  {
+    id: 54,
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    id: 55,
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: 56,
+    quote: "Don't be afraid to give up the good to go for the great.",
+    author: "John D. Rockefeller",
+  },
+  {
+    id: 57,
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 58,
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    id: 59,
+    quote:
+      "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Christian D. Larson",
+  },
+  {
+    id: 60,
+    quote:
+      "Every day may not be good, but there is something good in every day.",
+    author: "Unknown",
+  },
+  {
+    id: 61,
+    quote:
+      "The biggest risk is not taking any risk. In a world that's changing quickly, the only strategy that is guaranteed to fail is not taking risks.",
+    author: "Mark Zuckerberg",
+  },
+  {
+    id: 62,
+    quote: "The best preparation for tomorrow is doing your best today.",
+    author: "H. Jackson Brown Jr.",
+  },
+  {
+    id: 63,
+    quote: "Don't let yesterday take up too much of today.",
+    author: "Will Rogers",
+  },
+  {
+    id: 64,
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 65,
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 66,
+    quote: "When you focus on the good, the good gets better.",
+    author: "Abraham Hicks",
+  },
+  {
+    id: 67,
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: 68,
+    quote:
+      "Success is not the absence of failure; it's the persistence through failure.",
+    author: "",
+  },
+  {
+    id: 69,
+    quote: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe",
+  },
+  {
+    id: 70,
+    quote:
+      "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    author: "Roy T. Bennett",
+  },
+  {
+    id: 71,
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 72,
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    id: 73,
+    quote:
+      "Faith is taking the first step even when you don't see the whole staircase.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    id: 74,
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: 75,
+    quote: "Life is 10% what happens to us and 90% how we react to it.",
+    author: "Charles R. Swindoll",
+  },
+  {
+    id: 76,
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    id: 77,
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: 78,
+    quote: "Don't be afraid to give up the good to go for the great.",
+    author: "John D. Rockefeller",
+  },
+  {
+    id: 79,
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 80,
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    id: 81,
+    quote:
+      "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Christian D. Larson",
+  },
+  {
+    id: 82,
+    quote:
+      "Every day may not be good, but there is something good in every day.",
+    author: "Unknown",
+  },
+  {
+    id: 83,
+    quote:
+      "The biggest risk is not taking any risk. In a world that's changing quickly, the only strategy that is guaranteed to fail is not taking risks.",
+    author: "Mark Zuckerberg",
+  },
+  {
+    id: 84,
+    quote: "The best preparation for tomorrow is doing your best today.",
+    author: "H. Jackson Brown Jr.",
+  },
+  {
+    id: 85,
+    quote: "Don't let yesterday take up too much of today.",
+    author: "Will Rogers",
+  },
+  {
+    id: 86,
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 87,
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 88,
+    quote: "When you focus on the good, the good gets better.",
+    author: "Abraham Hicks",
+  },
+  {
+    id: 89,
+    quote: "Believe you can and you're halfway there.",
+    author: "",
+  },
+  {
+    id: 90,
+    quote:
+      "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer",
+  },
+  {
+    id: 91,
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: 92,
+    quote:
+      "The only person you should try to be better than is the person you were yesterday.",
+    author: "Unknown",
+  },
+  {
+    id: 93,
+    quote:
+      "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got this.",
+    author: "Chantal Sutherland",
+  },
+  {
+    id: 94,
+    quote:
+      "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 95,
+    quote: "Don't be afraid to give up the good to go for the great.",
+    author: "John D. Rockefeller",
+  },
+  {
+    id: 96,
+    quote:
+      "Hardships often prepare ordinary people for an extraordinary destiny.",
+    author: "C.S. Lewis",
+  },
+  {
+    id: 97,
+    quote:
+      "The biggest risk is not taking any risk. In a world that's changing quickly, the only strategy that is guaranteed to fail is not taking risks.",
+    author: "Mark Zuckerberg",
+  },
+  {
+    id: 98,
+    quote:
+      "Success is not the absence of failure; it's the persistence through failure.",
+    author: "Aisha Tyler",
+  },
+  {
+    id: 99,
+    quote:
+      "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
+  },
+  {
+    id: 100,
+    quote:
+      "The harder you work for something, the greater you'll feel when you achieve it.",
+    author: "Unknown",
   },
 ];
